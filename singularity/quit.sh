88a7a6b344024726e85b1f5c19ef936bccbf067b
Tashrif Billah <tbillah@bwh.harvard.edu>
2021-01-31 20:29:57 -0500

    remove obsolete comments

--- singularity/quit.sh
+++ singularity/quit.sh
@@ -7,16 +7,12 @@ set -eo pipefail
 cd /sw/apps/dpdash
 npm stop
 
-# todo: make this do a graceful exit
+# TODO: make this do a graceful exit
 
-# The commented line below is not working on Linux 7
-# ps auxww | grep 'pm2' | awk '{print $2}' | xargs kill -9
 pkill -f 'pm2'
 
-#todo: send SIGQUIT to supervisord 
+# TODO: send SIGQUIT to supervisord
 
-# The commented line below is not working on Linux 7
-# ps auxww | grep 'supervisord' | awk '{print $2}' | xargs kill -15
 pkill -f 'supervisord'
 pkill -f 'rabbitmq'
 pkill -f 'erlang'

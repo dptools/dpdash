8c1a91e81d3d4602b0828089e4316c9828fb4bcb
Ben Silverman <BLSilverman@mgh.harvard.edu>
2021-05-06 17:18:03 -0400

    Use generated /dist for setup.sh
    The mounted /dist will be used for running DPDash with configs.

--- singularity/init.sh
+++ singularity/init.sh
@@ -137,7 +137,6 @@ echo '***************Setting up DPdash*****************'
 singularity exec \
 -B ${containerDataDir}:/data \
 -B ${containerDataDir}/dpdash/configs/dashboard:/sw/apps/dpdash/server/configs \
--B ${containerDataDir}/dpdash/dist:/sw/apps/dpdash/dist \
 -B ${dataDir}:/project_data \
 $DPDASH_IMG \
 /data/scripts/setup.sh $mongopw $mongoPort $rabbitpw $appsecret

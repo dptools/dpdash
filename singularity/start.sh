9bed9cb9203b19949a137726d28450bc92e9ef9d
Ben Silverman <BLSilverman@mgh.harvard.edu>
2021-05-11 15:05:37 -0400

    start.sh: move err check and allow -d without -s

--- singularity/start.sh
+++ singularity/start.sh
@@ -10,11 +10,16 @@ while getopts ":sd" opt; do
     case $opt in
         s)
             shellmode=true
-            echo "Starting Singularity shell..."
             ;;
         d)
-            devmode=true
-            echo "Mounting dev directory..."
+            if [ -z ${DPDASH_DEV_DIR} ] || [ ! -d "$DPDASH_DEV_DIR" ]
+            then 
+                echo "DPDASH_DEV_DIR is unset in .env or is not a directory"
+                exit 1
+            else
+                devmode=true
+                echo "Mounting dev directory..."
+            fi
             ;;
         \?)
             echo "Invalid option: -$OPTARG"
@@ -28,21 +33,16 @@ source ./varcheck.sh
 
 if [ $shellmode = true ]
 then
+    echo "Starting Singularity shell..."
     if [ $devmode = true ]
     then
-        if [ -z ${DPDASH_DEV_DIR} ] || [ ! -d "$DPDASH_DEV_DIR" ]
-        then 
-            echo "DPDASH_DEV_DIR is unset in .env or is not a directory"
-            exit 1
-        else
-            singularity shell \
-            -B ${state}:/data \
-            -B ${data}:/project_data \
-            -B ${DPDASH_DEV_DIR}:/sw/apps/dpdash \
-            -B ${state}/dpdash/configs/dashboard:/sw/apps/dpdash/server/configs \
-            -B ${state}/dpdash/dist:/sw/apps/dpdash/dist \
-            ${DPDASH_IMG}
-        fi
+        singularity shell \
+        -B ${state}:/data \
+        -B ${data}:/project_data \
+        -B ${DPDASH_DEV_DIR}:/sw/apps/dpdash \
+        -B ${state}/dpdash/configs/dashboard:/sw/apps/dpdash/server/configs \
+        -B ${state}/dpdash/dist:/sw/apps/dpdash/dist \
+        ${DPDASH_IMG}
     else
         singularity shell \
         -B ${state}:/data \
@@ -52,11 +52,24 @@ then
         ${DPDASH_IMG}
     fi
 else
-    singularity run \
-    -B ${state}:/data \
-    -B ${data}:/project_data \
-    -B ${state}/dpdash/configs/dashboard:/sw/apps/dpdash/server/configs \
-    -B ${state}/dpdash/dist:/sw/apps/dpdash/dist \
-    ${DPDASH_IMG} \
-    /sw/apps/dpdash/singularity/run.sh
+    echo "Starting Singularity..."
+    if [ $devmode = true ]
+    then
+        singularity run \
+        -B ${state}:/data \
+        -B ${data}:/project_data \
+        -B ${DPDASH_DEV_DIR}:/sw/apps/dpdash \
+        -B ${state}/dpdash/configs/dashboard:/sw/apps/dpdash/server/configs \
+        -B ${state}/dpdash/dist:/sw/apps/dpdash/dist \
+        ${DPDASH_IMG} \
+        /sw/apps/dpdash/singularity/run.sh
+    else
+        singularity run \
+        -B ${state}:/data \
+        -B ${data}:/project_data \
+        -B ${state}/dpdash/configs/dashboard:/sw/apps/dpdash/server/configs \
+        -B ${state}/dpdash/dist:/sw/apps/dpdash/dist \
+        ${DPDASH_IMG} \
+        /sw/apps/dpdash/singularity/run.sh
+    fi
 fi

f29c87f399d8eb1a8c8cc0e0cfb1e53864dba8c6
Ben Silverman <BLSilverman@mgh.harvard.edu>
2021-06-07 15:15:30 -0400

    Add reusable utils fetchUtil and countUtil

---
+++ views/fe-utils/fetchUtil.js
@@ -0,0 +1,55 @@
+const fetchStudies = async () => {
+  const res = await window.fetch('/api/v1/search/studies', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'same-origin'
+  })
+  if (res.status !== 200) {
+    throw new Error(res.statusText);
+  }
+  return res.json();
+}
+
+const fetchSubjects = async () => {
+  const userAccess = await window.fetch('/api/v1/studies', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'same-origin'
+  });
+  if (userAccess.status !== 200) {
+    throw new Error(userAccess.statusText);
+  }
+  const studiesJson = await userAccess.json();
+  const studies = studiesJson ? studiesJson : [];
+  const subjectsResponse = await window.fetch('/api/v1/subjects?q=' + JSON.stringify(studies), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'same-origin',
+  })
+  if (subjectsResponse.status !== 200) {
+    throw new Error(subjectsResponse.statusText);
+  }
+  return subjectsResponse.json();
+};
+
+const fetchUsers = async () => {
+  const res = await window.fetch('/api/v1/users', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'same-origin'
+  })
+  if (res.status !== 200) {
+    throw new Error(res.statusText);
+  }
+  return res.json()
+};
+
+export { fetchStudies, fetchSubjects, fetchUsers };

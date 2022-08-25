c7e791b909899e941569a06d4497a6261218a725
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-08-25 10:49:46 -0500

    Tooltip text
    * Updated text to display on tooltip
    * Decreased tooltip font size

--- views/components/Graphs/BarGraph.jsx
+++ views/components/Graphs/BarGraph.jsx
@@ -8,10 +8,12 @@ import {
   VictoryLegend,
   VictoryLabel,
   VictoryVoronoiContainer,
+  VictoryTooltip,
 } from 'victory'
 
-import { graphStyles } from '../../styles/chart_styles'
 import { colors } from '../../../constants/styles'
+import { graphStyles } from '../../styles/chart_styles'
+import { toolTipPercent } from '../../fe-utils/tooltipUtil'
 
 const NOT_AVAILABLE = 'N/A'
 const TOTALS_STUDY = 'Totals'
@@ -25,15 +27,27 @@ const BarGraph = ({ graph }) => {
       containerComponent={
         <VictoryVoronoiContainer
           labels={({ datum: { study, studyTarget, count, valueLabel } }) => {
+            const { targetTotal, count: studyTotalCount } =
+              graph.studyTotals[study]
             const showToolTip =
               study &&
               count &&
               study !== TOTALS_STUDY &&
               valueLabel !== NOT_AVAILABLE
             return showToolTip
-              ? `Site:${study} \n Current: ${count} \n Target: ${studyTarget}`
+              ? `${study} target: ${targetTotal} (100%) \n ${study} current: ${studyTotalCount} (${toolTipPercent(
+                  studyTotalCount,
+                  targetTotal
+                )}%)\n ${valueLabel} target: ${studyTarget} (${toolTipPercent(
+                  +studyTarget,
+                  targetTotal
+                )}%) \n ${valueLabel} current: ${count} (${toolTipPercent(
+                  count,
+                  targetTotal
+                )}%)`
               : null
           }}
+          labelComponent={<VictoryTooltip style={{ fontSize: 7 }} />}
         />
       }
     >
---
+++ views/fe-utils/tooltipUtil.js
@@ -0,0 +1,2 @@
+export const toolTipPercent = (count, targetTotal) =>
+  ((count / targetTotal) * 100).toFixed(0)

6f496d4344737b752f8d7f45d30936bd778ce5c4
Mike Stone <mikestone14@gmail.com>
2022-08-31 12:25:41 -0400

    Render a label and tooltip
    This commit creates a custom label component, `LabelWithTooltip`. The label
    is always rendered, which displays a percentage value within the bar chart
    stack element. The tooltip is only rendered when hovering over a stack and
    it displays more details about that stack's data.

--- views/components/Graphs/BarGraph.jsx
+++ views/components/Graphs/BarGraph.jsx
@@ -19,8 +19,53 @@ const NOT_AVAILABLE = 'N/A'
 const TOTALS_STUDY = 'Totals'
 const DEFAULT_ZOOM = 6
 
+const tooltipText = (graph, datum) => {
+  const { study, studyTarget, count, valueLabel } = datum
+  if (graph.studyTotals[study]) {
+    const { targetTotal, count: studyTotalCount } = graph.studyTotals[study]
+    const showToolTip =
+      study && count && study !== TOTALS_STUDY && valueLabel !== NOT_AVAILABLE
+    return showToolTip
+      ? `${study} target: ${targetTotal} (100%)\n${study} current: ${studyTotalCount} (${toolTipPercent(
+          studyTotalCount,
+          targetTotal
+        )}%)\n${valueLabel} target: ${studyTarget} (${toolTipPercent(
+          studyTarget,
+          targetTotal
+        )}%)\n${valueLabel} current: ${count} (${toolTipPercent(
+          count,
+          targetTotal
+        )}%)`
+      : null
+  }
+}
+
+const LabelWithTooltip = ({ hoverData, ...props }) => {
+  const isTooltipActive =
+    !!hoverData &&
+    hoverData.group === props.datum._group &&
+    hoverData.stack === props.datum._stack &&
+    hoverData.text
+
+  return (
+    <g>
+      <VictoryLabel
+        {...props}
+        dy={15}
+        style={{ fill: colors.anti_flash_white, fontSize: 7 }}
+      />
+      <VictoryTooltip
+        {...props}
+        text={hoverData?.text}
+        active={isTooltipActive}
+        style={{ fontSize: 7, textAnchor: 'start', padding: 5 }}
+      />
+    </g>
+  )
+}
 
 const BarGraph = ({ graph }) => {
+  const [hoverData, setHoverData] = React.useState()
   const siteDataPerChartValue = Object.values(graph.data)
   const numSitesPerValue = siteDataPerChartValue.map((value) => value.length)
   const numSites = Math.max(...numSitesPerValue)
@@ -43,7 +88,7 @@ const BarGraph = ({ graph }) => {
         orientation="horizontal"
         gutter={20}
         data={graph.legend}
-        x={150}
+        x={125}
         y={20}
         labelComponent={<VictoryLabel />}
       />
@@ -54,7 +99,7 @@ const BarGraph = ({ graph }) => {
         style={graphStyles.yAxis}
         tickFormat={(yAxisValue) => `${yAxisValue}%`}
       />
-      <VictoryStack>
+      <VictoryStack labelComponent={<LabelWithTooltip hoverData={hoverData} />}>
         {siteDataPerChartValue.map((data, idx) => (
           <VictoryBar
             data={data}
@@ -69,13 +114,6 @@ const BarGraph = ({ graph }) => {
             labels={({ datum }) =>
               !!datum?.percent ? `${datum.percent.toFixed(0)}%` : null
             }
-            labelComponent={
-              <VictoryLabel
-                dy={15}
-                labelPlacement="perpendicular"
-                style={{ fill: colors.anti_flash_white, fontSize: 8 }}
-              />
-            }
             events={[
               {
                 target: 'data',
@@ -84,7 +122,14 @@ const BarGraph = ({ graph }) => {
                     return [
                       {
                         mutation: (props) => {
+                          setHoverData({
+                            group: props.datum._group,
+                            stack: props.datum._stack,
+                            text: tooltipText(graph, props.datum),
+                          })
+
                           return {
+                            ...props,
                             style: {
                               ...props.style,
                               stroke: colors.black,
@@ -96,6 +141,7 @@ const BarGraph = ({ graph }) => {
                     ]
                   },
                   onMouseOut: () => {
+                    setHoverData(undefined)
                     return [
                       {
                         mutation: () => {

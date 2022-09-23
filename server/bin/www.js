82b123a11b8e6c7bebe4ebaf463fe15293842236
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2022-09-23 15:52:23 -0400

    Locate importer.js (#181)
    * Fixes error on cron job when looking for importer.js script

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,59 +1,60 @@
-
 /**
  * Module dependencies.
  */
 
-import app from '../app';
-import { createServer } from 'http';
-import { connect } from 'amqplib/callback_api';
-import { CronJob } from 'cron';
-import { spawn } from 'child_process';
+import app from '../app'
+import { createServer } from 'http'
+import { connect } from 'amqplib/callback_api'
+import { CronJob } from 'cron'
+import { spawn } from 'child_process'
+import path from 'path'
+import fs from 'fs'
 
-const debug = require('debug')('po:server');
+const debug = require('debug')('po:server')
 
-import config from '../configs/config';
+import config from '../configs/config'
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(config.app.port);
-app.set('port', port);
+const port = normalizePort(config.app.port)
+app.set('port', port)
 
-console.log(port);
+console.log(port)
 
 /**
  * Create HTTP server.
  */
 
-const server = createServer(app);
+const server = createServer(app)
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port, config.app.address);
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen(port, config.app.address)
+server.on('error', onError)
+server.on('listening', onListening)
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  var port = parseInt(val, 10)
 
   if (isNaN(port)) {
     // named pipe
-    return val;
+    return val
   }
 
   if (port >= 0) {
     // port number
-    return port;
+    return port
   }
 
-  return false;
+  return false
 }
 
 /**
@@ -62,91 +63,99 @@ function normalizePort(val) {
 var io = require('socket.io')(server, {
   transports: ['websocket', 'polling'],
   pingInterval: 1500,
-  pingTimeout: 2000
-}); //squid v2
+  pingTimeout: 2000,
+}) //squid v2
 
-app.set('socketio', io);
+app.set('socketio', io)
 
 /* authenticated */
 io.use(function (socket, next) {
-  if (socket.request.headers.cookie) return next();
-  next(new Error('Authentication error'));
-});
+  if (socket.request.headers.cookie) return next()
+  next(new Error('Authentication error'))
+})
 
 /* Not authenticated */
 io.on('connection', function (socket) {
   socket.on('subscribe', function (roomID) {
-    socket.join(roomID);
-  });
+    socket.join(roomID)
+  })
   socket.on('unsubscribe', function (roomID) {
-    socket.leave(roomID);
-  });
-});
+    socket.leave(roomID)
+  })
+})
 
 /**
  * Create a rabbitmq connection
  */
-var amqpAddress = 'amqp://' + config.rabbitmq.username;
-amqpAddress = amqpAddress + ':' + config.rabbitmq.password;
-amqpAddress = amqpAddress + '@' + config.rabbitmq.host + ':' + config.rabbitmq.port;
+var amqpAddress = 'amqp://' + config.rabbitmq.username
+amqpAddress = amqpAddress + ':' + config.rabbitmq.password
+amqpAddress =
+  amqpAddress + '@' + config.rabbitmq.host + ':' + config.rabbitmq.port
 connect(amqpAddress, config.rabbitmq.opts, function (err, conn) {
-  if (err) console.log(err);
+  if (err) console.log(err)
   conn.createChannel(function (err, ch) {
-    if (err) console.log(err);
-    ch.assertQueue(config.rabbitmq.consumerQueue, { durable: false }, function (err, q) {
-      if (err) console.log(err);
-      consumer(conn, ch, q.queue);
-    });
-  });
-});
-
+    if (err) console.log(err)
+    ch.assertQueue(
+      config.rabbitmq.consumerQueue,
+      { durable: false },
+      function (err, q) {
+        if (err) console.log(err)
+        consumer(conn, ch, q.queue)
+      }
+    )
+  })
+})
 
 /*
-* Set up crons for data and acl import
-*/
+ * Set up crons for data and acl import
+ */
 for (const hour in config.rabbitmq.sync.hours) {
   if (config.rabbitmq.sync.hours[hour] < 10) {
-    var syncTime = '00 00 0' + config.rabbitmq.sync.hours[hour] + ' * * 0-6';
+    var syncTime = '00 00 0' + config.rabbitmq.sync.hours[hour] + ' * * 0-6'
   } else {
-    var syncTime = '00 00 ' + config.rabbitmq.sync.hours[hour] + ' * * 0-6';
+    var syncTime = '00 00 ' + config.rabbitmq.sync.hours[hour] + ' * * 0-6'
   }
-  new CronJob({
-    cronTime: syncTime,
-    onTick: function () {
-      var importer = spawn('node', ['./utils/importer.js']);
 
-      importer.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-      });
+  const importerPath = path.join(__dirname, '..', 'utils', 'importer.js')
 
-      importer.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
-      });
+  if (fs.existsSync(importerPath)) {
+    new CronJob({
+      cronTime: syncTime,
+      onTick: function () {
+        var importer = spawn('node', [importerPath])
 
-      importer.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-      });
-    },
-    start: true,
-    timeZone: config.rabbitmq.sync.timezone
-  });
+        importer.stdout.on('data', (data) => {
+          console.log(`stdout: ${data}`)
+        })
+
+        importer.stderr.on('data', (data) => {
+          console.log(`stderr: ${data}`)
+        })
+
+        importer.on('close', (code) => {
+          console.log(`child process exited with code ${code}`)
+        })
+      },
+      start: true,
+      timeZone: config.rabbitmq.sync.timezone,
+    })
+  }
 }
 
 /* socket.io connection */
-var dashboardNsp = io.of('/dashboard');
+var dashboardNsp = io.of('/dashboard')
 
 function consumer(conn, ch, replyTo) {
   ch.consume(replyTo, function (msg) {
-    var response = JSON.parse(msg.content.toString());
-    var taskStatus = response.status;
+    var response = JSON.parse(msg.content.toString())
+    var taskStatus = response.status
     if (taskStatus === 'PROCESSING') {
-      dashboardNsp.emit(taskStatus, { taskId: response.task_id });
+      dashboardNsp.emit(taskStatus, { taskId: response.task_id })
     } else if (taskStatus === 'SUCCESS') {
-      dashboardNsp.emit(taskStatus, { taskId: response.task_id });
+      dashboardNsp.emit(taskStatus, { taskId: response.task_id })
     }
-  });
+  })
 }
-
 
 /**
  * Event listener for HTTP server "error" event.
@@ -154,25 +163,23 @@ function consumer(conn, ch, replyTo) {
 
 function onError(error) {
   if (error.syscall !== 'listen') {
-    throw error;
+    throw error
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
     default:
-      throw error;
+      throw error
   }
 }
 
@@ -181,9 +188,7 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  var addr = server.address()
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  debug('Listening on ' + bind)
 }

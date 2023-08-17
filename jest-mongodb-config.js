616851e38f7316d4bdf3739d7f7ff08629b45ab2
Jack Curtis <jack@gnar.dog>
2023-08-17 11:04:52 -0400

    add in-memory mongodb for testing

---
+++ jest-mongodb-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mongodbMemoryServerOptions: {
+    binary: {
+      skipMD5: true,
+    },
+    instance: {},
+    autoStart: false,
+  },
+  mongoURLEnvName: 'MONGODB_URI',
+};

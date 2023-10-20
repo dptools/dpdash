5fe10fb1ef11e8c84a053ae78da05d2a13b95ab5
Jack Curtis <jack.v.curtis@gmail.com>
2023-10-20 09:46:54 -0400

    424 add cdk for deployment (#446)

---
+++ AWS_SETUP.md
@@ -0,0 +1,108 @@
+### 1. Request DNS Certificate
+
+For this step you will require two things:
+
+1. A domain name for the DP Dash application
+2. The ability to set CNAME records for that domain which reference AWS DNS names
+
+Log in to your AWS console and navigate to the Certificate Manager service.
+
+![Screenshot of the AWS Certificate Manager link](/doc/assets/aws_setup/01_request_certificate//create_certificate_01.png)
+
+Click the Request button in the top right corner.
+
+![Screenshot of the Request Certificate button](/doc/assets/aws_setup/01_request_certificate//create_certificate_02.png)
+
+Select "Request a public certificate".
+
+![Screenshot of the Request Certificate form](/doc/assets/aws_setup/01_request_certificate//create_certificate_04.png)
+
+Enter the fully qualified domain name at which the DP Dash app will be hosted, e.g. `dpdash.mydomain.com` if using a subdomain. Leave all other settings unchanged and click "Request".
+
+AWS will provide a CNAME record to add to your DNS records. Set this record in the management system for your domain and AWS will automatically verify it and create your certificate.
+
+![Screenshot of CNAME values for certificate verification](/doc/assets/aws_setup/01_request_certificate//create_certificate_05.png)
+
+Copy the ARN of the issued certificate and save it locally. We will use it when configuring the deployment for your environment.
+
+![Screenshot of ARN for certificate](/doc/assets/aws_setup/01_request_certificate//create_certificate_06.png)
+
+### 2. Create AWS Role for Github Actions
+
+Navigate to the IAM service in the AWS Console.
+
+![Screenshot of the IAM link](/doc/assets/aws_setup/02_create_github_role/create_github_role_01.png)
+
+Select "Roles" from the IAM Dashboard and click the Create button.
+
+![Screenshot of IAM dashboard](/doc/assets/aws_setup/02_create_github_role/create_github_role_02.png)
+
+For Trusted Entity select "Web Identity". For Identity Provider select "token.actions.githubusercontent.com". For Audience select "sts.amazonaws.com". Enter your Github Organization Name and the repo you wish to grant access to. Click "Create".
+
+![Screenshot of role creation form with values filled out](/doc/assets/aws_setup/02_create_github_role/create_github_role_03.png)
+
+Copy the ARN of the new role and save it locally. We will use it when configuring the deployment for your environment.
+
+### 3. Bootstrap the CDK and Generate Secrets
+
+This step will only need to be completed if the CDK has not been bootstrapped for your account/AWS environment. Ensure you are logged in via the AWS CLI. From the root of the project directory run:
+
+```bash
+npm i
+npm run bootstrap
+```
+
+Create secret values in AWS encrypted SSM Parameter store:
+
+```bash
+export DPDASH_MONGODB_ADMIN_USER=dpdash
+export DPDASH_IMPORT_API_USERS=dev-api-user
+./bin/generate-secrets.sh
+```
+
+Secret values can be retrieved and decryted via the `./bin/echo-secrets.sh` script. THIS WILL LOG SECRET VALUES TO THE CONSOLE.
+
+### 4. Build and Deploy Docker Images
+
+Run the following commands from the root of this repository:
+
+```bash
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account")
+docker pull mongo:5.0.21
+docker build -t dpdash:latest .
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
+docker tag mongo:5.0.21 $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/mongo:5.0.21
+docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/mongo:5.0.21
+
+docker tag dpdash:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/dpdash:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/dpdash:latest
+```
+
+### 5. Set Github Action Variables and Deploy
+
+Navigate to your Github repository and select the Settings tab. Open the "Secrets and Variables" menu on the left-hand side and select "Actions", then "New Variable".
+
+![Screenshot of the Github Settings Menu](/doc/assets/aws_setup/04_set_github_variables/set_github_variables_01.png)
+
+![Screenshot of Github Actions Variables](/doc/assets/aws_setup/04_set_github_variables/set_github_variables_02.png)
+
+Create 2 variables.
+
+1. Variable Name: CERT_ARN, Variable Value: ARN of your AWS Certificate from Step 1.
+2. Variable Name: ROLE_ARN, Variable Value: ARN of your Role from Step 2.
+
+Navigate to the Actions tab and the Deploy Infrastructure workflow. Select "Run Workflow". The application will deploy.
+
+![Screenshot of Actions tab with Run Workflow button](/doc/assets/aws_setup/04_set_github_variables/set_github_variables_03.png)
+
+### 6. Check the Deployment and Set CNAME Record
+
+To verify that the application is deployed, view the DNS name with the following command:
+
+```bash
+aws elbv2 describe-load-balancers --names dpDashDevLoadBalancer --query "LoadBalancers[0].DNSName"
+```
+
+Visit this domain in your browser via `https://`. You will get a warning from your browser that the certificate is incorrect. This is the expected behavior and means that the server is responding appropriately. It is not necessary to override the browser warning
+
+You may then set a CNAME record in your DNS system for the intended domain of this application, and direct requests to the URL retrieved from the above command.
---
+++ cdk_lib/dpdash-cdk-stack.ts
@@ -0,0 +1,184 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as efs from "aws-cdk-lib/aws-efs";
+import * as ssm from "aws-cdk-lib/aws-ssm";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as certificate_manager from "aws-cdk-lib/aws-certificatemanager";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import { Construct } from 'constructs';
+
+const APP_NAME = "DpDash";
+
+export class DpdashCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    if (process.env.DEV_CERT_ARN) {
+      const devCertificatArn = process.env.DEV_CERT_ARN;
+
+      const secrets = {
+        mongoDbUserDev: ecs.Secret.fromSsmParameter(ssm.StringParameter.fromSecureStringParameterAttributes(this, `${APP_NAME}MongoDbUser`, {
+          parameterName: 'DPDASH_MONGODB_ADMIN_USER_DEV',
+          version: 1,
+        })),
+        mongoDbPasswordDev: ecs.Secret.fromSsmParameter(ssm.StringParameter.fromSecureStringParameterAttributes(this, `${APP_NAME}MongoDbPassword`, {
+          parameterName: 'DPDASH_MONGODB_ADMIN_PASSWORD_DEV',
+          version: 1,
+        })),
+        sessionSecretDev: ecs.Secret.fromSsmParameter(ssm.StringParameter.fromSecureStringParameterAttributes(this, `${APP_NAME}SessionDevSecret`, {
+          parameterName: 'DPDASH_SESSION_SECRET_DEV',
+          version: 1,
+        })),
+        importApiUsersDev: ecs.Secret.fromSsmParameter(ssm.StringParameter.fromSecureStringParameterAttributes(this, `${APP_NAME}ImportApiUsers`, {
+          parameterName: 'DPDASH_IMPORT_API_USERS_DEV',
+          version: 1,
+        })),
+        importApiKeysDev: ecs.Secret.fromSsmParameter(ssm.StringParameter.fromSecureStringParameterAttributes(this, `${APP_NAME}ImportApiKeysDev`, {
+          parameterName: 'DPDASH_IMPORT_API_KEYS_DEV',
+          version: 1,
+        })),
+      }
+      const vpc = new ec2.Vpc(this, `${APP_NAME}Vpc`, {
+        availabilityZones: [`${cdk.Aws.REGION}a`, `${cdk.Aws.REGION}b`],
+      });
+  
+      const efsSecurityGroup = new ec2.SecurityGroup(this,`${APP_NAME}DevEfsSg`, {
+        vpc,
+      });
+  
+      const mongoRepository = ecr.Repository.fromRepositoryName(this, `${APP_NAME}DevMongoRepository`, "mongo");
+      const dpdashRepository = ecr.Repository.fromRepositoryName(this, `${APP_NAME}DevDpdashRepository`, "dpdash");
+  
+      const mongoTaskDefinition = new ecs.FargateTaskDefinition(this, `${APP_NAME}DevMongoTaskDefinition`, {
+        memoryLimitMiB: 512,
+        cpu: 256,
+      });
+  
+      const fileSystem = new efs.FileSystem(this, `${APP_NAME}DevEfsFileSystem`, { 
+        vpc,
+        securityGroup: efsSecurityGroup,
+        encrypted: true,
+        vpcSubnets: {
+          subnets: vpc.privateSubnets,
+        },
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+       });
+  
+      const accessPoint = new efs.AccessPoint(this, `${APP_NAME}DevVolumeAccessPoint`,  {
+        fileSystem: fileSystem,
+     })
+  
+      mongoTaskDefinition.addVolume({
+        name: "mongo-data",
+        efsVolumeConfiguration: {
+          fileSystemId: fileSystem.fileSystemId,
+          authorizationConfig: {
+            accessPointId: accessPoint.accessPointId,
+            iam: "ENABLED", 
+          },
+          transitEncryption: "ENABLED",
+        },
+      });
+  
+      mongoTaskDefinition.addToTaskRolePolicy(
+        new iam.PolicyStatement({
+          actions: [
+            'elasticfilesystem:ClientRootAccess',
+            'elasticfilesystem:ClientWrite',
+            'elasticfilesystem:ClientMount',
+            'elasticfilesystem:DescribeMountTargets',
+          ],
+          resources: [
+            fileSystem.fileSystemArn,
+          ],
+        })
+      );
+      
+      mongoTaskDefinition.addToTaskRolePolicy(
+        new iam.PolicyStatement({
+          actions: ['ec2:DescribeAvailabilityZones'],
+          resources: ['*'],
+        })
+      );
+      
+      const mongoContainer = mongoTaskDefinition.addContainer(`${APP_NAME}DevMongoContainer`, {
+        image: ecs.ContainerImage.fromEcrRepository(mongoRepository, "5.0.21"),
+        portMappings: [{ containerPort: 27017 }],        
+        logging: ecs.LogDrivers.awsLogs({ streamPrefix: `${APP_NAME}DevMongoContainer` }),
+        secrets: {
+          MONGO_INITDB_ROOT_USERNAME: secrets.mongoDbUserDev,
+          MONGO_INITDB_ROOT_PASSWORD: secrets.mongoDbPasswordDev,
+        },
+      });
+  
+      mongoContainer.addMountPoints({
+        containerPath: "/data/db",
+        readOnly: false,
+        sourceVolume: "mongo-data",
+      });
+  
+      const mongoService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, `${APP_NAME}MongoService`, {
+        vpc: vpc,
+        cpu: 512,
+        taskDefinition: mongoTaskDefinition,
+        taskSubnets: {
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        listenerPort: 27017,
+        maxHealthyPercent: 100,
+        minHealthyPercent: 0,
+        desiredCount: 1,
+        assignPublicIp: false,
+        publicLoadBalancer: false,
+      });
+  
+  
+      mongoService.targetGroup.configureHealthCheck({
+        port: "27017",
+      });
+  
+      
+      efsSecurityGroup.addIngressRule(
+          mongoService.service.connections.securityGroups[0],
+          ec2.Port.tcp(2049) // Enable NFS service within security group
+      )
+      
+      new ecs_patterns.ApplicationLoadBalancedFargateService(this, `${APP_NAME}DevAppService`, {
+        vpc: vpc,
+        serviceName: 'dpDashDevService',
+        loadBalancerName: 'dpDashDevLoadBalancer',
+        cluster: new ecs.Cluster(this, `${APP_NAME}DevCluster`, {
+          clusterName: 'dpDashDevCluster',
+        }),
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromEcrRepository(dpdashRepository, "latest"),
+          containerPort: 8000,
+          environment: {
+            MONGODB_HOST: mongoService.loadBalancer.loadBalancerDnsName,
+          },
+          secrets: {
+            MONGODB_USER: secrets.mongoDbUserDev,
+            MONGODB_PASSWORD: secrets.mongoDbPasswordDev,
+            SESSION_SECRET: secrets.sessionSecretDev,
+            IMPORT_API_USERS: secrets.importApiUsersDev,
+            IMPORT_API_KEYS: secrets.importApiKeysDev,
+          },
+          logDriver: ecs.LogDrivers.awsLogs({ streamPrefix: `${APP_NAME}DevAppContainer` }),
+        },
+        taskDefinition: new ecs.FargateTaskDefinition(this, `${APP_NAME}DevAppTaskDefinition`, {
+          family: 'dpDashDevTaskDefinition',
+        }),
+        assignPublicIp: true,
+        publicLoadBalancer: true,
+        redirectHTTP: true,
+        certificate: certificate_manager.Certificate.fromCertificateArn(this, `${APP_NAME}DevCertificate`, devCertificatArn),
+        taskSubnets: {
+          subnets: vpc.publicSubnets.concat(vpc.privateSubnets),
+        },
+      });
+    } else {
+      throw new Error("CertificateArn is not defined")
+    }
+  }
+}

e2174a501ed0dfa060e1959ca0f618d334f9a3bb
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2024-01-10 13:06:35 -0600

    ensure admin fix (#567)
    * Fix ensure admin

--- server/utils/passport/ensure-admin.js
+++ server/utils/passport/ensure-admin.js
@@ -8,12 +8,8 @@ export default async function ensureAdmin(req, res, next) {
     const { appDb } = req.app.locals
     const query = { uid: req.user, role: 'admin' }
     const user = await UserModel.findOne(appDb, query)
-    const isOwnAdminAccount = req?.params?.uid === user?.uid
 
     if (!user) return res.status(401).json({ error: 'Incorrect access level.' })
-
-    if (isOwnAdminAccount)
-      return res.status(401).json({ error: 'Cannot modify your own account.' })
 
     return next()
   } catch (error) {

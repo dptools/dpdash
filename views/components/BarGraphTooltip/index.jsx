dd38dcb9df1ba230590cb9a10bcd67839e6e677f
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2024-03-21 10:35:49 -0600

    :`# This is a combination of 2 commits. (#703)
    Tooltip
    
    * tooltip style updates
    
    Style updates
    
    * set height to 3px

--- views/components/BarGraphTooltip/index.jsx
+++ views/components/BarGraphTooltip/index.jsx
@@ -13,26 +13,32 @@ const BarGraphTooltip = ({
 }) => {
   if (!active || !payload?.length) return null
 
+  const siteName = useSiteName ? label : SITE_NAMES[label]
+  const countsTotal = useSiteName
+    ? studyTotals[label].count
+    : studyTotals[SITE_NAMES[label]].count
+  const variableCounts = payload[0].payload.counts
+
   return (
     <div className="BarGraphTooltip">
       {displayLeft ? <div className="arrow-left" /> : null}
       <div className="BarGraphTooltip_data">
-        <div className="BarGraphTooltip_label">{label}</div>
+        <div className="BarGraphTooltip_label">{siteName}</div>
         <div className="BarGraphTooltip_values">Value</div>
-        {payload.map(({ name, value }) => {
+        <span className="BarGraphTooltip_divider" />
+        {payload.map(({ name }) => {
           return (
             <Fragment key={name}>
               <div className="BarGraphTooltip_label">{name}</div>
-              <div className="BarGraphTooltip_values">{value}</div>
+              <div className="BarGraphTooltip_values">
+                {variableCounts[name]}
+              </div>
             </Fragment>
           )
         })}
+        <span className="BarGraphTooltip_divider" />
         <div className="BarGraphTooltip_label">Total</div>
-        <div className="BarGraphTooltip_values">
-          {useSiteName
-            ? studyTotals[label]?.count || null
-            : studyTotals[SITE_NAMES[label]]?.count || null}
-        </div>
+        <div className="BarGraphTooltip_values">{countsTotal}</div>
       </div>
       {!displayLeft ? <div className="arrow-right" /> : null}
     </div>
